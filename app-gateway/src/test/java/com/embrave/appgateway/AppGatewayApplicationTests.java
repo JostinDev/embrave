a3ad29e801f2e97dab79cb2415eb0ada25f4f0ba
Justin Nydegger <justin.nydegger@adnovum.ch>
2023-08-31 07:56:15 +0200

    Init project

---
+++ app-gateway/src/test/java/com/embrave/appgateway/AppGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.embrave.appgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
