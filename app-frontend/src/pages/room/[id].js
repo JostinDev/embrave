a99a2ebe328bc692afefd3d28fe46c08004372f9
JostinDev <justinnydegger@hotmail.com>
2023-12-09 18:51:00 +0100

    Allow an admin to kick a user out of a room

--- app-frontend/src/pages/room/[id].js
+++ app-frontend/src/pages/room/[id].js
@@ -283,7 +283,7 @@ export default function Challenge() {
 			// TODO Let admins promote other users to admin
 			// TODO Mark a challenge as done
 			// TODO show image to be uploaded
-			// TODO kick someone from the room
+			// TODO TEST kick someone one of a room
 			<div className="min-h-screen bg-blue-500 pt-20">
 				<div className='mx-auto mt-10 p-10 rounded-md bg-white w-1/2 max-w-2xl'>
 					<h1 className='mb-10 text-2xl'>Post milestone</h1>
