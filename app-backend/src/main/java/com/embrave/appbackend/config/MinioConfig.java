2a89af502400b4aebac8bb53be842d63b0df0433
JostinDev <justinnydegger@hotmail.com>
2024-04-12 07:56:07 +0200

    Update Minio config with variables

--- app-backend/src/main/java/com/embrave/appbackend/config/MinioConfig.java
+++ app-backend/src/main/java/com/embrave/appbackend/config/MinioConfig.java
@@ -5,18 +5,35 @@ import io.minio.MakeBucketArgs;
 import io.minio.MinioClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
 public class MinioConfig {
+
+    @Value("${minio.host}")
+    private String host;
+
+    @Value("${minio.port}")
+    private int port;
+
+    @Value("${minio.user}")
+    private String user;
+
+    @Value("${minio.password}")
+    private String password;
+
     @Bean
     public MinioClient generateMinioClient() {
+
+
+
         try {
             MinioClient minioClient =
                     MinioClient.builder()
                             //TODO Needs to be changed with host ip
                             //.endpoint("192.168.1.55", 9000, false)
-                            .endpoint("localhost", 9000, false)
-                            .credentials("admin", "adminadmin")
+                            .endpoint(host, port, false)
+                            .credentials(user, password)
                             .build();
 
             boolean found = minioClient.bucketExists(BucketExistsArgs.builder().bucket("embrave").build());
