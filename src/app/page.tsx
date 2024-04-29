ebf0af6838989fc7bd8593d580048ee8aecd0c21
JostinDev <justinnydegger@hotmail.com>
2024-04-29 14:49:00 +0200

    Fix login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,59 +1,61 @@
 import Link from 'next/link';
-import { notFound } from 'next/navigation';
-import { ClerkLoaded, SignedIn, SignedOut, SignInButton } from '@clerk/nextjs';
+import { SignInButton } from '@clerk/nextjs';
+import { auth } from '@clerk/nextjs/server';
 
 import ChallengeCard from '@/components/challengeCard';
 import { getUserRoom } from '@/server/queries';
 
 export default async function Index() {
-  const rooms = await getUserRoom();
+  const { userId } = auth();
 
+  if (!userId) {
+    return (
+      <div className="relative">
+        <div className={'flex flex-col'}>
+          <p className="text-title2 mb-2 text-sand-12">You are not logged in</p>
+          <SignInButton mode="modal">
+            <button
+              className={'text-body-l-book h-fit max-w-fit rounded-lg bg-sand-12 p-3 text-sand-3'}
+            >
+              Login
+            </button>
+          </SignInButton>
+        </div>
+      </div>
+    );
+  }
+
+  const rooms = await getUserRoom();
   return (
     <div className="relative">
-      <ClerkLoaded>
-        <SignedIn>
-          <div>
-            <p className="text-title2 mb-4 text-sand-12">Currently Active Challenges</p>
-            <div className="flex flex-wrap gap-4">
-              {rooms
-                ? rooms.map((room) => {
-                    if (room && room.challenge) {
-                      let date =
-                        room.created.getDate() +
-                        '.' +
-                        room.created.getMonth() +
-                        '.' +
-                        room.created.getFullYear();
-                      return (
-                        <Link key={room.id} href={`/room/${room.id}`}>
-                          <ChallengeCard
-                            id={room.id}
-                            challenge={room.challenge.title}
-                            date={date}
-                            type={room.challenge.type.type}
-                            streak={0}
-                          ></ChallengeCard>
-                        </Link>
-                      );
-                    }
-                  })
-                : ''}
-            </div>
-          </div>
-        </SignedIn>
-        <SignedOut>
-          <div className={'flex flex-col'}>
-            <p className="text-title2 mb-2 text-sand-12">You are not logged in</p>
-            <SignInButton mode="modal">
-              <button
-                className={'text-body-l-book h-fit max-w-fit rounded-lg bg-sand-12 p-3 text-sand-3'}
-              >
-                Login
-              </button>
-            </SignInButton>
-          </div>
-        </SignedOut>
-      </ClerkLoaded>
+      <div>
+        <p className="text-title2 mb-4 text-sand-12">Currently Active Challenges</p>
+        <div className="flex flex-wrap gap-4">
+          {rooms
+            ? rooms.map((room) => {
+                if (room && room.challenge) {
+                  let date =
+                    room.created.getDate() +
+                    '.' +
+                    room.created.getMonth() +
+                    '.' +
+                    room.created.getFullYear();
+                  return (
+                    <Link key={room.id} href={`/room/${room.id}`}>
+                      <ChallengeCard
+                        id={room.id}
+                        challenge={room.challenge.title}
+                        date={date}
+                        type={room.challenge.type.type}
+                        streak={0}
+                      ></ChallengeCard>
+                    </Link>
+                  );
+                }
+              })
+            : ''}
+        </div>
+      </div>
     </div>
   );
 }
