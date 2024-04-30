8f670407971883bad1e5e944da8ced9f6c4967d1
Igor Gassmann <igor@igassmann.me>
2024-04-30 16:42:15 +0200

    refactor: remove custom CSS classes

--- src/components/challengeComplete/ChallengeCompleteCard.tsx
+++ src/components/challengeComplete/ChallengeCompleteCard.tsx
@@ -6,12 +6,11 @@ import { Button, Dialog, DialogTrigger, Heading, Modal } from 'react-aria-compon
 import ChallengeCompleteTracker from '@/components/challengeComplete/ChallengeCompleteTracker';
 import { setChallengeDone } from '@/server/mutations';
 
-import './style.css';
-
 type SharePopoverProps = {
   roomID: number;
   isChallengeDone: boolean;
 };
+
 export default function ChallengeCompleteCard(props: SharePopoverProps) {
   const [isChallengeDone, setIsMilestoneDone] = useState(props.isChallengeDone);
   const [isOpen, setOpen] = React.useState(false);
@@ -31,16 +30,16 @@ export default function ChallengeCompleteCard(props: SharePopoverProps) {
   return (
     <div
       className={
-        'w-100 border-green-4 bg-green-2 relative mx-auto mb-6 flex max-w-[700px] items-center justify-between overflow-hidden rounded-[26px] border p-8'
+        'w-100 relative mx-auto mb-6 flex max-w-[700px] items-center justify-between overflow-hidden rounded-[26px] border border-green-4 bg-green-2 p-8'
       }
     >
       {isChallengeDone && (
         <div
-          className={'confetti animate-confetti absolute left-0 top-0 h-16 w-full bg-repeat-x'}
+          className={'confetti absolute left-0 top-0 h-16 w-full animate-confetti bg-repeat-x'}
         />
       )}
       <div>
-        <p className={'text-title1 text-green-11 mb-2'}>
+        <p className={'text-title1 mb-2 text-green-11'}>
           {isChallengeDone ? 'Congrats! You completed the challenge!' : 'Completed the Challenge?'}
         </p>
         <p className={'text-body-l-book text-green-11'}>
@@ -53,8 +52,13 @@ export default function ChallengeCompleteCard(props: SharePopoverProps) {
         <Button onPress={() => !isChallengeDone && setOpen(true)}>
           <ChallengeCompleteTracker roomID={props.roomID} isChallengeDone={isChallengeDone} />
         </Button>
-        <Modal isDismissable isOpen={isOpen} onOpenChange={setOpen}>
-          <Dialog>
+        <Modal
+          isDismissable
+          className="shadow:[0px_8px_20px_rgba(0,0,0/0.1)] w-[90%] max-w-[480px] rounded-2xl border border-sand-5 bg-sand-1 p-6"
+          isOpen={isOpen}
+          onOpenChange={setOpen}
+        >
+          <Dialog className="outline-none">
             {({ close }) => (
               <form>
                 <Heading className={'text-title1 mb-4 text-sand-12'} slot="title">
