6490a243c48a6c7319aa0be0204b0977d843b5c1
JostinDev <justinnydegger@hotmail.com>
2024-06-10 09:11:12 +0200

    Fix: Remove bug when "/" wasn't accessible when accessing the url

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,28 +1,9 @@
 import Link from 'next/link';
-import { SignInButton } from '@clerk/nextjs';
-import { auth } from '@clerk/nextjs/server';
 
 import ChallengeCard from '@/app/(app)/components/ChallengeCard';
 import { getRoomStreak, getUserRoom } from '@/server/queries';
 
 export default async function Index() {
-  const { userId } = auth();
-
-  if (!userId) {
-    return (
-      <div className="relative">
-        <div className="flex flex-col">
-          <p className="mb-2 font-nexa text-xl font-bold text-sand-12">You are not logged in</p>
-          <SignInButton mode="modal">
-            <button className="h-fit max-w-fit rounded-lg bg-sand-12 p-3 font-inter text-base leading-18 text-sand-3">
-              Sign In
-            </button>
-          </SignInButton>
-        </div>
-      </div>
-    );
-  }
-
   const [completedRooms, incompletedRooms] = await getUserRoom();
   return (
     <div className="relative">
