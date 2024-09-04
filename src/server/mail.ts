f910bc3931297120ecbe983ec6ee43d69867a832
JostinDev <justinnydegger@hotmail.com>
2024-09-04 22:44:35 +0200

    Test sending emails

--- src/server/mail.ts
+++ src/server/mail.ts
@@ -3,7 +3,11 @@ import formdata from 'form-data';
 import Mailgun from 'mailgun.js';
 
 const mailgun = new Mailgun(formdata);
-const mg = mailgun.client({ username: 'api', key: process.env.MAILGUN_SECRET || '' });
+const mg = mailgun.client({
+  username: 'api',
+  key: process.env.MAILGUN_SECRET || '',
+  url: 'https://api.eu.mailgun.net',
+});
 
 export async function sendBuyConfirmationPremium(to: string) {
   auth().protect();
