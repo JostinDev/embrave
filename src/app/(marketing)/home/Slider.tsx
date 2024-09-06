3517a9838e9af69ce874661ae4d2004d98ff64d8
JostinDev <justinnydegger@hotmail.com>
2024-09-06 15:39:15 +0200

    Hero slider

---
+++ src/app/(marketing)/home/Slider.tsx
@@ -0,0 +1,67 @@
+'use client';
+
+import { useEffect } from 'react';
+import Image from 'next/image';
+import gsap from 'gsap';
+
+import ellipseBlue from '@/app/(marketing)/home/images/ellipseBlue.svg';
+import ellipseEnd from '@/app/(marketing)/home/images/ellipseEnd.svg';
+import ellipseGreen from '@/app/(marketing)/home/images/ellipseGreen.svg';
+import ellipseOrange from '@/app/(marketing)/home/images/ellipseOrange.svg';
+import ellipsePurple from '@/app/(marketing)/home/images/ellipsePurple.svg';
+
+export default function Slider() {
+  useEffect(() => {
+    gsap.to('.slide1', { y: 100, duration: 0.7, opacity: 0, delay: 2 });
+    gsap.fromTo('.slide2', { y: -100, opacity: 0 }, { y: 0, opacity: 1, duration: 0.7, delay: 2 });
+    gsap.to('.slide2', { y: 100, duration: 0.7, opacity: 0, delay: 4 });
+
+    gsap.fromTo('.slide3', { y: -100, opacity: 0 }, { y: 0, opacity: 1, duration: 0.7, delay: 4 });
+    gsap.to('.slide3', { y: 100, duration: 0.7, opacity: 0, delay: 6 });
+
+    gsap.fromTo('.slide4', { y: -100, opacity: 0 }, { y: 0, opacity: 1, duration: 0.7, delay: 6 });
+    gsap.to('.slide4', { y: 100, duration: 0.7, opacity: 0, delay: 8 });
+
+    gsap.fromTo('.slide5', { y: -100, opacity: 0 }, { y: 0, opacity: 1, duration: 0.7, delay: 8 });
+  }, []);
+
+  return (
+    <div className="relative mx-auto flex max-w-[820px] justify-center pt-64">
+      <div className="slide1 absolute mx-auto flex items-center justify-center">
+        <Image src={ellipseOrange} alt="ellipse" className="absolute z-10 " />
+        <p className="right-1/2 top-1/2 z-20 mx-auto text-center font-nexa text-5xl font-bold text-orange-9">
+          <span className="font-sourceSerif4 font-light italic">Climb </span>a mountain?
+        </p>
+      </div>
+
+      <div className="slide2 absolute mx-auto flex max-w-[820px] items-center justify-center opacity-0 ">
+        <Image src={ellipsePurple} alt="ellipse" className="absolute z-10" />
+        <p className="right-1/2 top-1/2 z-20 mx-auto text-center font-nexa text-5xl font-bold text-purple-9">
+          Find new <span className="font-sourceSerif4 font-light italic">friends</span>?
+        </p>
+      </div>
+
+      <div className="slide3 absolute mx-auto flex max-w-[820px] items-center justify-center opacity-0 ">
+        <Image src={ellipseGreen} alt="ellipse" className="absolute z-10" />
+        <p className="right-1/2 top-1/2 z-20 mx-auto text-center font-nexa text-5xl font-bold text-jade-9">
+          Cold plunges <span className="font-sourceSerif4 font-light italic">every day</span>?
+        </p>
+      </div>
+
+      <div className="slide4 absolute mx-auto flex max-w-[820px] items-center justify-center opacity-0 ">
+        <Image src={ellipseBlue} alt="ellipse" className="absolute z-10" />
+        <p className="text-blue-9 right-1/2 top-1/2 z-20 mx-auto text-center font-nexa text-5xl font-bold">
+          <span className="font-sourceSerif4 font-light italic">No social media </span>for a week?
+        </p>
+      </div>
+
+      <div className="slide5 absolute mx-auto flex max-w-[820px] items-center justify-center opacity-0 ">
+        <Image src={ellipseEnd} alt="ellipse" className="absolute z-10 " />
+        <p className="right-1/2 top-1/2 z-20 mx-auto text-center font-nexa text-5xl font-bold text-sand-12">
+          Challenge yourself and change{' '}
+          <span className="font-sourceSerif4 font-light italic">your life!</span>
+        </p>
+      </div>
+    </div>
+  );
+}
--- src/app/(marketing)/home/page.tsx
+++ src/app/(marketing)/home/page.tsx
@@ -1,45 +1,39 @@
 import Header from '@/app/(marketing)/home/Header';
 import LandingCard from '@/app/(marketing)/home/LandingCard';
 import Separator from '@/app/(marketing)/home/Separator';
+import Slider from '@/app/(marketing)/home/Slider';
 
 export default function LandingPage() {
   return (
     <div>
-      <div className="container mx-auto px-4 pb-20 pt-60">
+      <div className="container mx-auto px-4 pb-20">
         <Header />
-
-        <p className="mx-auto mb-12 max-w-[1000px] text-center font-nexa text-7xl font-extrabold text-sand-12">
+        <Slider />
+        <p className="mx-auto mb-12 max-w-[1000px] pt-[400px] text-center font-nexa text-7xl font-extrabold text-sand-12">
           Staying in your <span className="font-sourceSerif4 font-light italic">comfort zone?</span>
           Not today.
         </p>
-
         <LandingCard
           title="Built for change"
           content="We value self development. That is why we created Embrave, so that you can start challenges to push yourself outside your comfort zone."
         />
-
         <Separator />
-
-        <p className="mx-auto mb-12 max-w-[1000px] text-center font-nexa text-7xl font-extrabold text-sand-12">
+        <div className="mx-auto mb-12 max-w-[1000px] text-center font-nexa text-7xl font-extrabold text-sand-12">
           <p>
             In it with your <span className="font-sourceSerif4 font-light italic">friends.</span>
           </p>
           <p>
             Or just <span className="font-sourceSerif4 font-light italic">on your own.</span>
           </p>
-        </p>
-
+        </div>
         <LandingCard
           title="Sharing experiences"
           content="Sometimes is nice to have friends to hold you accountable. Other times you just want to be on your own. With Embrave you can do both."
         />
-
         <Separator />
-
         <p className="mx-auto mb-12 max-w-[1000px] text-center font-nexa text-7xl font-extrabold text-sand-12">
           You are on <span className="font-sourceSerif4 font-light italic">fire!</span>
         </p>
-
         <LandingCard
           title="Habit building"
           content="With Embrave you can track your progress easily, so that you really follow through with your challenge."
