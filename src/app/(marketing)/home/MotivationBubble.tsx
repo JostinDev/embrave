510dd89ef7f5d831c7f3a550580c96fc1761a011
JostinDev <justinnydegger@hotmail.com>
2024-09-11 17:16:17 +0200

    Add missing text and fix images

--- src/app/(marketing)/home/MotivationBubble.tsx
+++ src/app/(marketing)/home/MotivationBubble.tsx
@@ -2,27 +2,38 @@
 
 import Image from 'next/image';
 
+import ellipseAwesome from '@/app/(marketing)/home/images/ellipseAwesome.svg';
 import ellipseLookAtMe from '@/app/(marketing)/home/images/ellipseLookAtMe.svg';
 import ellipseNoRun from '@/app/(marketing)/home/images/ellipseNoRun.svg';
 
 export default function MotivationBubble() {
   return (
     <div className="relative mb-52 mt-40 w-full">
-      <div className="mx-auto flex items-center justify-center md:ml-60">
+      <div className="mx-auto flex items-center justify-center md:mr-72">
+        <Image src={ellipseAwesome} alt="ellipse" className="absolute z-10" />
+        <div className="z-20 mx-auto text-center md:text-start">
+          <p className="mb-1 font-sourceSerif4 text-4xl font-light italic text-orange-9 sm:text-5xl">
+            &quot;Awesome!&quot;
+          </p>
+          <p className="font-nexa text-base font-bold text-orange-8">Your gym partner</p>
+        </div>
+      </div>
+
+      <div className="mx-auto mt-20 flex items-center justify-center md:ml-60 md:mt-8">
         <Image src={ellipseNoRun} alt="ellipse" className="absolute z-10" />
-        <div className="z-20 mx-auto">
+        <div className="z-20 mx-auto text-center md:text-start">
           <p className="mb-1 font-sourceSerif4 text-4xl font-light italic text-purple-9 sm:text-5xl">
             No run today?
           </p>
           <p className="font-nexa text-base font-bold text-purple-8">
-            Your best friend preparing for a marathon.
+            Your best friend preparing for a marathon
           </p>
         </div>
       </div>
 
-      <div className="mx-auto mt-14 flex items-center justify-center md:mr-60">
+      <div className="mx-auto mt-20 flex items-center justify-center md:mr-60 md:mt-10">
         <Image src={ellipseLookAtMe} alt="ellipse" className="absolute z-10" />
-        <div className="z-20 mx-auto">
+        <div className="z-20 mx-auto text-center md:text-start">
           <p className="mb-1 font-sourceSerif4 text-4xl font-light italic text-sky-11 sm:text-5xl">
             “Look at me go!”
           </p>
