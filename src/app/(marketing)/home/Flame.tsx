264c4582dd99ce3911805a821455983bc5004b5d
JostinDev <justinnydegger@hotmail.com>
2024-09-13 16:35:18 +0200

    Adjust flame height

--- src/app/(marketing)/home/Flame.tsx
+++ src/app/(marketing)/home/Flame.tsx
@@ -20,7 +20,7 @@ export default function Flame() {
   return (
     <div className="relative mx-auto mb-64 mt-36 flex flex-wrap justify-center gap-32 md:flex-nowrap md:gap-4">
       <div className="flame1 relative top-1/2 mx-auto flex w-full justify-center opacity-0">
-        <Image src={ellipseStreak} alt="ellipse" className="absolute" />
+        <Image src={ellipseStreak} alt="ellipse" className="absolute -top-[50px]" />
         <div className="right-1/2 top-1/2 z-20 mx-auto text-center font-nexa text-4xl font-bold text-orange-9 sm:text-5xl">
           <Image src={flame} alt="flame" className="mx-auto mb-5" />
           Day one
@@ -29,7 +29,7 @@ export default function Flame() {
       </div>
 
       <div className="flame2 relative top-1/2 mx-auto flex w-full justify-center opacity-0">
-        <Image src={ellipseStreak} alt="ellipse" className="absolute" />
+        <Image src={ellipseStreak} alt="ellipse" className="absolute -top-[50px]" />
         <div className="right-1/2 top-1/2 z-20 mx-auto text-center font-nexa text-4xl font-bold text-orange-9 sm:text-5xl">
           <Image src={flame} alt="flame" className="mx-auto mb-5" />
           Day two
@@ -38,7 +38,7 @@ export default function Flame() {
       </div>
 
       <div className="flame3 relative top-1/2 mx-auto flex w-full justify-center opacity-0">
-        <Image src={ellipseStreak} alt="ellipse" className="absolute" />
+        <Image src={ellipseStreak} alt="ellipse" className="absolute -top-[50px]" />
         <div className="right-1/2 top-1/2 z-20 mx-auto text-center font-nexa text-4xl font-bold text-orange-9 sm:text-5xl">
           <Image src={flame} alt="flame" className="mx-auto mb-5" />
           Day three
