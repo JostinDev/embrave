9bebdc16212f03bffa4f81ae055a56108cf8b1cc
JostinDev <justinnydegger@hotmail.com>
2024-09-13 16:59:33 +0200

    Add animation on the hero text

--- src/app/(marketing)/home/Slider.tsx
+++ src/app/(marketing)/home/Slider.tsx
@@ -5,7 +5,10 @@ import Image from 'next/image';
 import gsap from 'gsap';
 
 import ellipseBlue from '@/app/(marketing)/home/images/ellipseBlue.svg';
-import ellipseEnd from '@/app/(marketing)/home/images/ellipseEnd.svg';
+import ellipseEndBlue from '@/app/(marketing)/home/images/ellipseEndBlue.svg';
+import ellipseEndGreen from '@/app/(marketing)/home/images/ellipseEndGreen.svg';
+import ellipseEndOrange from '@/app/(marketing)/home/images/ellipseEndOrange.svg';
+import ellipseEndPurple from '@/app/(marketing)/home/images/ellipseEndPurple.svg';
 import ellipseGreen from '@/app/(marketing)/home/images/ellipseGreen.svg';
 import ellipseOrange from '@/app/(marketing)/home/images/ellipseOrange.svg';
 import ellipsePurple from '@/app/(marketing)/home/images/ellipsePurple.svg';
@@ -23,6 +26,20 @@ export default function Slider() {
     gsap.to('.slide4', { duration: 0.7, opacity: 0, delay: 8 });
 
     gsap.fromTo('.slide5', { opacity: 0 }, { y: 0, opacity: 1, duration: 0.7, delay: 8 });
+
+    const tl = gsap.timeline({ repeat: -1 });
+    tl.fromTo('.ellipse1', { opacity: 1 }, { y: 0, opacity: 0, duration: 2 });
+
+    tl.fromTo('.ellipse2', { opacity: 0 }, { y: 0, opacity: 1, duration: 2, delay: -2 });
+    tl.fromTo('.ellipse2', { opacity: 1 }, { y: 0, opacity: 0, duration: 2 });
+
+    tl.fromTo('.ellipse3', { opacity: 0 }, { y: 0, opacity: 1, duration: 2, delay: -2 });
+    tl.fromTo('.ellipse3', { opacity: 1 }, { y: 0, opacity: 0, duration: 2 });
+
+    tl.fromTo('.ellipse4', { opacity: 0 }, { y: 0, opacity: 1, duration: 2, delay: -2 });
+    tl.fromTo('.ellipse4', { opacity: 1 }, { y: 0, opacity: 0, duration: 2 });
+
+    tl.fromTo('.ellipse1', { opacity: 0 }, { y: 0, opacity: 1, duration: 2, delay: -2 });
   }, []);
 
   return (
@@ -50,13 +67,17 @@ export default function Slider() {
 
       <div className="slide4 absolute top-[45%] mx-auto flex w-full max-w-[820px] items-center justify-center opacity-0 ">
         <Image src={ellipseBlue} alt="ellipse" className="absolute z-10" />
-        <p className="text-blue-9 right-1/2 top-1/2 z-20 mx-auto text-center font-nexa text-4xl font-bold sm:text-5xl">
+        <p className="right-1/2 top-1/2 z-20 mx-auto text-center font-nexa text-4xl font-bold text-blue-9 sm:text-5xl">
           <span className="font-sourceSerif4 font-light italic">No social media </span>for a week?
         </p>
       </div>
 
       <div className="slide5 absolute top-[40%] mx-auto flex w-full max-w-[820px] items-center justify-center opacity-0 ">
-        <Image src={ellipseEnd} alt="ellipse" className="absolute z-10 " />
+        <Image src={ellipseEndOrange} alt="ellipse" className="ellipse1 absolute z-10" />
+        <Image src={ellipseEndPurple} alt="ellipse" className="ellipse2 absolute z-10" />
+        <Image src={ellipseEndGreen} alt="ellipse" className="ellipse3 absolute z-10" />
+        <Image src={ellipseEndBlue} alt="ellipse" className="ellipse4 absolute z-10" />
+
         <p className="right-1/2 top-1/2 z-20 mx-auto text-center font-nexa text-5xl font-bold text-sand-12">
           Challenge yourself and change{' '}
           <span className="font-sourceSerif4 font-light italic">your life!</span>
